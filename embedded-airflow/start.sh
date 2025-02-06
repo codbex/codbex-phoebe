e84357d614e2c65b9c159538c8703d1c059c48ba
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-02-06 12:29:56 +0200

    remove commented code - manual airflow commands from start.sh

--- embedded-airflow/start.sh
+++ embedded-airflow/start.sh
@@ -2,49 +2,6 @@
 
 # Start Airflow in the background
 echo "Current user: $(whoami)"
-#echo "Env: $(env)"
-
-#echo "Airflow DB migrate..."
-#airflow db migrate
-#
-#echo "Creating admin user..."
-#airflow users create \
-#          --username ${_AIRFLOW_WWW_USER_USERNAME:-admin1234} \
-#          --password ${_AIRFLOW_WWW_USER_USERNAME:-admin1234} \
-#          --firstname FIRST_NAME \
-#          --lastname LAST_NAME \
-#          --role Admin \
-#          --email admin@example.org
-
-#airflow standalone &
-#airflow standalone
-
-
-## Initialize the database
-#echo "Init DB..."
-#/usr/bin/dumb-init -- /entrypoint db init
-
-# Create an admin user if not exists
-#echo "Creating default user..."
-#/usr/bin/dumb-init -- /entrypoint users create \
-#    --username admin \
-#    --password admin \
-#    --firstname Admin \
-#    --lastname User \
-#    --role Admin \
-#    --email admin@example.com || echo "Admin user already exists"
-
-#export AIRFLOW__WEBSERVER__AUTHENTICATE=False
-#export AIRFLOW__WEBSERVER__AUTHORIZE=False
-#export AIRFLOW__WEBSERVER__RBAC=False
-#export AIRFLOW__WEBSERVER__AUTH_BACKEND=airflow.api.auth.backend.default
-#
-#echo "Starting Airflow Webserver..."
-#/usr/bin/dumb-init -- /entrypoint webserver --port 8080 &
-#
-## Start Airflow Scheduler
-#echo "Starting Airflow Scheduler..."
-#/usr/bin/dumb-init -- /entrypoint scheduler
 
 echo "Starting Airflow..."
 /usr/bin/dumb-init -- /entrypoint standalone &
