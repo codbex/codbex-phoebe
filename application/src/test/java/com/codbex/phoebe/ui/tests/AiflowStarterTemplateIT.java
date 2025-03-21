00199062463fdfd9627d691bd0c46c2d70a21938
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-21 14:20:56 +0200

    add Apache Airflow Starter template IT

---
+++ application/src/test/java/com/codbex/phoebe/ui/tests/AiflowStarterTemplateIT.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package com.codbex.phoebe.ui.tests;
+
+import org.eclipse.dirigible.tests.WelcomeView;
+import org.eclipse.dirigible.tests.Workbench;
+import org.eclipse.dirigible.tests.framework.HtmlAttribute;
+import org.eclipse.dirigible.tests.framework.HtmlElementType;
+import org.eclipse.dirigible.tests.restassured.RestAssuredExecutor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class AiflowStarterTemplateIT extends PhoebeIntegrationTest {
+
+    private static final String TEMPLATE_TITLE = "Apache Airflow Starter";
+    private static final String TEST_PROJECT = AiflowStarterTemplateIT.class.getSimpleName();
+
+    @Autowired
+    private RestAssuredExecutor restAssuredExecutor;
+
+    @Test
+    void testCreateFromTemplate() {
+        ide.openHomePage();
+        Workbench workbench = ide.openWorkbench();
+
+        WelcomeView welcomeView = workbench.openWelcomeView();
+        welcomeView.searchForTemplate(TEMPLATE_TITLE);
+        welcomeView.selectTemplate(TEMPLATE_TITLE);
+
+        welcomeView.typeProjectName(TEST_PROJECT);
+        welcomeView.typeFileName(TEST_PROJECT);
+        browser.enterTextInElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.ID, "param_dagId", TEST_PROJECT);
+        welcomeView.confirmTemplate();
+
+        workbench.publishAll(true);
+    }
+}
