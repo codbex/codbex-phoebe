abc1d77e735806316c4bc39bcadac19e74dccb3e
StanGenchev <82379274+StanZGenchev@users.noreply.github.com>
2025-03-24 08:30:41 +0200

    Proposal - Airflow view as perspective (#36)
    * Airflow view as perspective
    
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>
    
    * Removed unnecessary config line
    
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>
    
    * Changed config data name
    
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>
    
    * Disabled autofocus tab
    
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>
    
    * fix src
    
    ---------
    
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>
    Co-authored-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/ide/ide-ui-airflow/src/main/resources/META-INF/dirigible/ide-airflow/configs/airflow-perspective.js
+++ components/ide/ide-ui-airflow/src/main/resources/META-INF/dirigible/ide-airflow/configs/airflow-perspective.js
@@ -1,22 +1,23 @@
 /*
- * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ * Copyright (c) 2025 codbex or an codbex affiliate company and contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-FileCopyrightText: 2025 codbex or an codbex affiliate company and contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 const viewData = {
 	id: 'airflow',
-	region: 'bottom',
 	label: 'Airflow',
+	path: '/services/web/ide-airflow/index.html',
 	lazyLoad: true,
 	autoFocusTab: false,
-	path: '/services/web/ide-airflow/airflow.html'
+	icon: '/services/web/ide-airflow/images/airflow.svg',
+	order: 110,
 };
 if (typeof exports !== 'undefined') {
-	exports.getView = () => viewData;
+	exports.getPerspective = () => viewData;
 }
